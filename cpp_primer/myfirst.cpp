3b29145c7bf37a6244c5b1514f98e5afb13fd04a
halu886 <halu886@qq.com>
2019-04-01 19:49:02 +0800

    simon

--- cpp_primer/myfirst.cpp
+++ cpp_primer/myfirst.cpp
@@ -3,14 +3,17 @@
 int main()
 {
     using namespace std;
-
-    double area;
-    cout << "Enter the floor area, in square feet, of your home;";
-    cin >> area;
-    double side;
-    side = sqrt(area);
-    cout << "That`s the equivalent of a square" << side
-         << "feet to the side." << endl;
-    cout << "How fascinating" << endl;
+    simon(3);
+    cout << "Pick an integer";
+    int count;
+    cin >> count;
+    simon(count);
+    cout << "Done!" << endl;
     return 0;
+}
+
+void simon(int n)
+{
+    using namespace std;
+    cout << "Simon says touch your toes" << n << "times." << endl;
 }
