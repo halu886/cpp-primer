25e33a668a334e9ecd4070be0d9dd112efc21fbe
halu886 <halu886@qq.com>
2019-04-01 19:00:09 +0800

    cin

--- cpp_primer/myfirst.cpp
+++ cpp_primer/myfirst.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 int main() {
 	using namespace std;
-	cout << "Come up and C++ me some time,";
+	/*cout << "Come up and C++ me some time,";
 	cout << endl;
 	cout << "You won`t regret it!" << endl;
+	cin.get();
+	return 0;*/
+	int carrots;
+	cout << "How many carrots do you have?" << endl;
+	cin >> carrots;
+	cout << "Here are two more.";
+	carrots = carrots + 2;
+	cout << "Now you have " << carrots << " carrots." << endl;
 	return 0;
 }
