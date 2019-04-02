767318e0176dd8f6d79a84d23091e2c4d1fea2b9
halu886 <halu886@qq.com>
2019-04-02 19:56:01 +0800

    int type

--- cpp_primer/myfirst.cpp
+++ cpp_primer/myfirst.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
-int simon(int);
+#include <climits>
 int main()
 {
     using namespace std;
-    simon(3);
-    cout << "Pick an integer";
-    int count;
-    cin >> count;
-    int pounds = simon(count);
-    cout << "Done!"
-         << "pounds" << endl;
-    return 0;
-}
+    int n_int = INT_MAX;
+    short n_short = SHRT_MAX;
+    long n_long = LONG_MAX;
+    long long n_llong = LLONG_MAX;
 
-int simon(int n)
-{
-    return 14 * n;
+    cout << "int" << sizeof(int) << "bytes." << endl;
+    cout << "short" << sizeof n_short << "bytes." << endl;
+    cout << "long is" << sizeof n_long << "bytes." << endl;
+    cout << "long long " << sizeof n_llong << "bytes." << endl;
+    cout << endl;
+
+    cout << n_int << endl;
+    cout << n_short << endl;
+    cout << n_long << endl;
+    cout << n_llong << endl;
+    cout << INT_MIN << endl;
+    cout << CHAR_BIT << endl;
+	system("pause");
+    return 0;
 }
