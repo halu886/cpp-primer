b7e5fd2e650253d15a202dee3e6c281481a97df0
halu886 <halu886@qq.com>
2019-04-08 19:54:06 +0800

    溢出

--- cpp_primer/myfirst.cpp
+++ cpp_primer/myfirst.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
+#define ZERO 0
 #include <climits>
 int main()
 {
     using namespace std;
-    int n_int = INT_MAX;
-    short n_short = SHRT_MAX;
-    long n_long = LONG_MAX;
-    long long n_llong = LLONG_MAX;
+    short sam = SHRT_MAX;
+    unsigned short sue = sam;
 
-    cout << "int" << sizeof(int) << "bytes." << endl;
-    cout << "short" << sizeof n_short << "bytes." << endl;
-    cout << "long is" << sizeof n_long << "bytes." << endl;
-    cout << "long long " << sizeof n_llong << "bytes." << endl;
-    cout << endl;
+    cout << "Sam has " << sam << " dollars and Sue has" << sue;
+    cout << "dollars deposited" << endl
+         << "Add $1 to each account." << endl
+         << "Now";
+    sam = sam + 1;
+    sue = sue + 1;
+    cout << "Sam has" << sam << " dollars and Sue has " << sue;
+    cout << " dollars deposited.\nPoor Sam!" << endl;
 
-    cout << n_int << endl;
-    cout << n_short << endl;
-    cout << n_long << endl;
-    cout << n_llong << endl;
-    cout << INT_MIN << endl;
-    cout << CHAR_BIT << endl;
-	system("pause");
+    sam = ZERO;
+	sue = ZERO;
+    cout << sam << sue;
+    sam = sam - 1;
+    sue = sue - 1;
+    cout << sam << sue;
+    cout << "dollars deposited" << endl;
+    system("pause");
     return 0;
 }
