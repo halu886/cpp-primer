324c30f19a0a14e01191a33313a5e7a0af51c149
halu886 <halu886@qq.com>
2019-04-08 20:27:30 +0800

    char和int

--- cpp_primer/myfirst.cpp
+++ cpp_primer/myfirst.cpp
@@ -3,12 +3,17 @@ using namespace std;
 int main()
 {
     using namespace std;
-    char ch;
+    char ch = 'M';
+    int i = ch;
+    cout << "ascII" << ch << "is" << i << endl;
 
-    cout << "enter" << endl;
-    cin >> ch;
-    cout << "Hola;";
-    cout << "Thank you for" << ch << endl;
+    cout << "add to code:" << endl;
+    ch = ch + 1;
+    i = ch;
+    cout << "ascII" << ch << "is" << i << endl;
+    cout << "Displaying char :";
+    cout.put(ch);
+    cout.put('|');
     system("pause");
     return 0;
 }
