c59f719b9e15f089a9db7a83d0ca630beb1884cc
halu886 <halu886@qq.com>
2019-04-16 20:01:47 +0800

    chars

--- cpp_primer/myfirst.cpp
+++ cpp_primer/myfirst.cpp
@@ -3,18 +3,16 @@ using namespace std;
 int main()
 {
     using namespace std;
-    const int size = 15;
-    char name1[size];
-    char name2[size] = "testtesetaaaaa";
+    const int ArSize = 20;
+    char name[ArSize];
+    char dessert[ArSize];
+    cin >> name ;
+	cout << endl;
+	cout << endl;
+    cin >> dessert ;
 
-    cout << "asfdasdf " << name2;
-    cout << "asdfsdafds?\n";
-    cin >> name1;
-    cout << "atweasdfdsfa " << name1 << "  ";
-    cout << "stringlen" << strlen(name1) << endl;
-    cout << "sizeOf(name)" << sizeof(name1) << endl;
-    name2[3] = '\0';
-    cout << name2;
+    cout << name << endl;
+    cout << dessert << endl;
     system("pause");
     return 0;
 }
