657f7405c47e99e9bc9041fcb8460d21e452beb0
halu886 <halu886@qq.com>
2019-04-16 19:29:22 +0800

    string

--- cpp_primer/myfirst.cpp
+++ cpp_primer/myfirst.cpp
@@ -3,23 +3,22 @@ using namespace std;
 int main()
 {
     using namespace std;
+    int yams[3];
+    yams[0] = 7;
+    yams[1] = 8;
+    yams[2] = 6;
+    int yamcosts[3] = {20, 30, 5};
 
-    int auks, bats, coots;
-
-    auks = 19.99 + 11.99;
-
-    bats = (int)19.99 + (int)11.99;
-    coots = int(19.99) + int(11.99);
-    cout << "auks = " << auks << ",bats= " << bats;
-
-    cout << ", coots = " << coots << endl;
-
-    char ch = 'Z';
-
-    cout << "The code for " << ch << " is ";
-    cout << int(ch) << endl;
-    cout << "yes";
-    cout << static_cast<int>(ch) << endl;
+    cout << "Total yams = ";
+    cout << yams[0] + yams[1] + yams[2] << endl;
+    cout << "the package with" << yams[1] << "yams costs";
+    cout << yamcosts[1] << " cents per yam.\n";
+    int total = yams[0] * yamcosts[0] + yams[1] * yamcosts[1];
+    total = total + yams[2] * yamcosts[2];
+    cout << "The total yam expense is" << total << "test\n";
+    cout << "size of" << sizeof yams;
+    cout << "bytes\n";
+    cout << "size of[0]" << yams[0] << "bytes";
     system("pause");
     return 0;
 }
