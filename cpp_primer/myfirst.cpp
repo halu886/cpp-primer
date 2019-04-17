bb99e72ab8f4876c1f4eecc49ca4667c8639146e
halu886 <halu886@qq.com>
2019-04-17 19:51:51 +0800

    strtype2

--- cpp_primer/myfirst.cpp
+++ cpp_primer/myfirst.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
     using namespace std;
-    int year;
+    char a[20];
+    char d[20] = "afsd";
 
-    cin >> year;
-    cout << "address";
-    char address[80];
-    cin.getline(address, 80);
-    cout << "year" << year << "address" << address;
+    string b;
+    string c = "sadfasdf";
+
+    cout << "a";
+    cin >> a;
+
+    cout << "b";
+    cin >> b;
+
+    cout << "a" << a << "b" << b << "c" << c << "d" << d;
+
+    cout << "d[2] " << d[2];
+    cout << "c[2] " << c[2];
+
     system("pause");
     return 0;
 }
