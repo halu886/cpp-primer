f639afae2a06e0e76efd78998189ec91a117edd0
halu886 <halu886@qq.com>
2019-04-22 19:29:56 +0800

    struct

--- cpp_primer/myfirst.cpp
+++ cpp_primer/myfirst.cpp
@@ -1,35 +1,27 @@
 #include <iostream>
 #include <string>
 using namespace std;
+struct inflatable
+{
+    char name[20];
+    float volume;
+    double price;
+};
+
 int main()
 {
     using namespace std;
+    inflatable douque = {
+        "weqrqe",
+        0.25,
+        132.43
+	};
+    cout << "price" << douque.name << endl;
+    cout << douque.price << endl;
 
-    string s1 = "asfasdfasdf";
-    string s2, s3;
-
-    cout << "=\n";
-    s2 = s1;
-
-    cout << "s1:" << s1 << "s2:" << s2 << endl;
-
-    s2 = "buuuuuuu";
-
-    cout << "s2: " << s2;
-
-    s3 = s1 + s2;
-
-    cout << "s3: " << s3;
-
-    cout << "+=\n";
-
-    s1 += s2;
-
-    cout << "s1: "<< s1;
-
-    s2 += "for a day";
-
-    cout << "s2" << s2 << endl;
+    inflatable choice = douque;
+    cout << "price" << choice.name << endl;
+    cout << choice.price << endl;
     system("pause");
     return 0;
 }
