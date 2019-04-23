3e3d0df257984e6809b68a37683bd7c591463460
halu886 <halu886@qq.com>
2019-04-23 19:18:02 +0800

    use_new

--- cpp_primer/myfirst.cpp
+++ cpp_primer/myfirst.cpp
@@ -5,14 +5,24 @@ using namespace std;
 int main()
 {
     using namespace std;
-    int higgens = 5;
-    int *pt = &higgens;
+    int inti = 1000;
 
-    cout << "higgens " << higgens
-         << " &higgens " << &higgens;
+    int *pi = new int;
+    *pi = 1001;
 
-    cout << "pt" << pt
-         << " *pt" << *pt;
+    cout << "v " << inti << " l " << &inti << endl;
+    cout << "v " << *pi << " l " << pi << endl;
+
+    double *pd = new double;
+    *pd = 1000000001.0;
+
+    cout << "v " << *pd << " l " << pd << endl;
+    cout << "vl " << &pd << endl;
+
+    cout << "sizeof pt" << sizeof(pi) << endl;
+    cout << "sizeof *pt" << sizeof(*pi) << endl;
+    cout << "sizeof pd" << sizeof(pd) << endl;
+    cout << "sizeof *pd" << sizeof(*pd) << endl;
     system("pause");
     return 0;
 }
