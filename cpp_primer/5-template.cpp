5329c75e55a8474fd86f0e5f7c4a4a783c33b593
halu886 <halu886@qq.com>
2019-07-11 20:03:08 +0800

    8.5 未完成

---
+++ cpp_primer/5-template.cpp
@@ -0,0 +1,7 @@
+template <typename AnyType>
+AnyType max(Array<AnyType> &arr) {
+	int max_p = 0;
+	for (int i = 0; i < 5; i++) {
+		
+	}
+}
--- cpp_primer/myfirst.cpp
+++ cpp_primer/myfirst.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 using namespace std;
 
+
+
 int main()
 {
     using namespace std;
